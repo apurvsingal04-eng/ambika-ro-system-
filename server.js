70b5013ab0ea091b6f2775b292ee09dbcb12413a
apurvsingal04-eng <apurvsingal04@gmail.com>
2026-08-08 06:13:29 +0000

    Add simple express static server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.use(express.static(path.join(__dirname, 'artifacts/ambika-ro-system')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'artifacts/ambika-ro-system', 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
